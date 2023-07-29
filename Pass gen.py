27f1df9c5f9add56a372210478cd0d0d1755b1af
franklinspablu <franklinspablu@gmail.com>
2023-07-29 03:33:51 -0500

    Message change

--- Pass gen.py
+++ Pass gen.py
@@ -2,15 +2,17 @@
 
 import random
 from typing import Generator 
-
+long = 0
 min= "abcdefghijklmnñopqrsuvwxyz"
 may= "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 num= "0123456789"
 sim= "[]{}()*;/,._-"
 
 res = min + may + num + sim
-lon = 16
-contraseña = "".join(random.sample(res,lon))
+print("How long do you want?")
+long = input("We recommend more than 16 characters")
+
+contraseña = "".join(random.sample(res,long))
 print (contraseña)
 
 
